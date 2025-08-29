a2391ccab404397883fcf129c49affce76e4ce31
Paul Bogdan <paulcbogdan@gmail.com>
2025-08-29 08:06:58 -0400

    Updated publications page and CV

--- _plugins/fix_bibliography_headers.rb
+++ _plugins/fix_bibliography_headers.rb
@@ -9,8 +9,9 @@ Jekyll::Hooks.register :pages, :post_render do |page|
     # Note: Jekyll Scholar converts 9997 to "submitted" automatically, so we replace that
     page.output = page.output.gsub('<h2 class="bibliography">9999</h2>', '<h2 class="bibliography">In Press</h2>')
     page.output = page.output.gsub('<h2 class="bibliography">9998</h2>', '<h2 class="bibliography">In Preparation</h2>')
-    page.output = page.output.gsub('<h2 class="bibliography">submitted</h2>', '<h2 class="bibliography">Preprints</h2>')  # Jekyll Scholar converts 9997 to "submitted"
-    page.output = page.output.gsub('<h2 class="bibliography">9997</h2>', '<h2 class="bibliography">Preprints</h2>')  # Just in case
+    # Add slight padding-bottom to Preprints header to prevent lowercase 'p' from touching the line
+    page.output = page.output.gsub('<h2 class="bibliography">submitted</h2>', '<h2 class="bibliography" style="padding-bottom: 0.15rem;">Preprints</h2>')  # Jekyll Scholar converts 9997 to "submitted"
+    page.output = page.output.gsub('<h2 class="bibliography">9997</h2>', '<h2 class="bibliography" style="padding-bottom: 0.15rem;">Preprints</h2>')  # Just in case
     page.output = page.output.gsub('<h2 class="bibliography">9996</h2>', '<h2 class="bibliography">Under Review</h2>')
     page.output = page.output.gsub('<h2 class="bibliography">9995</h2>', '<h2 class="bibliography">Accepted</h2>')
     page.output = page.output.gsub('<h2 class="bibliography">9994</h2>', '<h2 class="bibliography">Forthcoming</h2>')
